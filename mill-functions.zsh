3e66e19868bda2f361d6ea8cb8abb8ff91dcc920
Carlos de Paula <me@carlosedp.com>
2023-12-14 09:26:43 -0300

    Reformat

--- mill-functions.zsh
+++ mill-functions.zsh
@@ -1,4 +1,3 @@
-
 # This function is used by Zsh P10k prompt. To use, add `mill_version` in the `p10k.zsh` file:
 #       typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
 #       status # already exists
@@ -8,46 +7,46 @@
 #       )
 function prompt_mill_version() {
     local millver
-    if [ -f ".mill-version" ] ; then
+    if [ -f ".mill-version" ]; then
         millver="$(<.mill-version)"
     else
         # Check if this is a mill project
-        if [ -f "build.sc" ] ; then
-          if [ -f "mill" ] ; then
-            millver="$(./mill --version | head -1 | cut -d' ' -f5)"
-          elif [ -x "$(command -v mill)" ] >/dev/null 2>&1; then
-            millver="$(mill --version | head -1 | cut -d' ' -f5)"
-          else
-            millver="unknown"
-          fi
-        millver="$millver (create .mill-version)"
+        if [ -f "build.sc" ]; then
+            if [ -f "mill" ]; then
+                millver="$(./mill --version | head -1 | cut -d' ' -f5)"
+            elif [ -x "$(command -v mill)" ] >/dev/null 2>&1; then
+                millver="$(mill --version | head -1 | cut -d' ' -f5)"
+            else
+                millver="unknown"
+            fi
+            millver="$millver (create .mill-version)"
         else
-          return
+            return
         fi
     fi
     #If keepMajor is true, functions will only use major versions (no daily builds)
     keepMajorMillVersion=true
 
-    local cache_dir=${XDG_CACHE_HOME:-$HOME/.cache}/p10k-${(%):-%n}
+    local cache_dir=${XDG_CACHE_HOME:-$HOME/.cache}/p10k-${USER}
     mkdir -p "$cache_dir" # just ensuring that it exists
     local cache_file="$cache_dir/latest_mill_version"
     local cache_file_snap="$cache_dir/latest_mill_version_snapshot"
 
     # Keep cache for 24 hours
     local timeout_in_hours=24
-    local timeout_in_seconds=$(($timeout_in_hours*60*60))
+    local timeout_in_seconds=$(($timeout_in_hours * 60 * 60))
 
     # Get latest version from maven repo if cache is older than timeout
     if [[ ! (-f "$cache_file" && -f "$cache_file_snap" && $(($(date +%s) - $(stat -c '%Y' "$cache_file") < $timeout_in_seconds)) -gt 0) ]]; then
         local latest_mill_version_maven
-        latest_mill_version_maven=$(curl -sL https://repo1.maven.org/maven2/com/lihaoyi/mill-scalalib_2.13/maven-metadata.xml | grep "<version>" |grep -v "\-M" |tail -1 |sed -e 's/<[^>]*>//g' |tr -d " ")
-        echo "$latest_mill_version_maven" > "$cache_file_snap"
+        latest_mill_version_maven=$(curl -sL https://repo1.maven.org/maven2/com/lihaoyi/mill-scalalib_2.13/maven-metadata.xml | grep "<version>" | grep -v "\-M" | tail -1 | sed -e 's/<[^>]*>//g' | tr -d " ")
+        echo "$latest_mill_version_maven" >"$cache_file_snap"
         if [ "$keepMajorMillVersion" = true ]; then
             latest_mill_version_maven=$(echo "$latest_mill_version_maven" | cut -d- -f1)
         fi
 
         if [[ -n "$latest_mill_version_maven" ]]; then
-            echo "$latest_mill_version_maven" > "$cache_file"
+            echo "$latest_mill_version_maven" >"$cache_file"
         else
             touch "$cache_file"
         fi
@@ -57,8 +56,7 @@ function prompt_mill_version() {
     latest_mill_version=$(<"$cache_file")
     latest_mill_version_snap=$(<"$cache_file_snap")
 
-    if [[ -n "$latest_mill_version" && $millver =~ \d+.\d+.\d+-\d+-.* && "$millver" == "$latest_mill_version_snap" && "$millver" != $(echo "$latest_mill_version" | cut -d- -f1) ]]
-    then
+    if [[ -n "$latest_mill_version" && $millver =~ \d+.\d+.\d+-\d+-.* && "$millver" == "$latest_mill_version_snap" && "$millver" != $(echo "$latest_mill_version" | cut -d- -f1) ]]; then
         # Project uses a snapshot version which is up-to-date, show current version in yellow
         p10k segment -s "UP_TO_DATE" -f yellow -i '' -t "⇡ Mill $millver"
     elif [[ -n "$latest_mill_version" && $millver =~ \d+.\d+.\d+-\d+-.* && "$millver" != "$latest_mill_version_snap" && "$millver" != $(echo "$latest_mill_version" | cut -d- -f1) ]]; then
@@ -77,14 +75,14 @@ function prompt_mill_version() {
 # Usage: `millupd` or `millupd -s` to update with latest snapshot build
 millupd() {
     snapshotBuilds="${1:-true}"
-    if [ "$snapshotBuilds" = "-s" ] ; then
+    if [ "$snapshotBuilds" = "-s" ]; then
         keepMajorMillVersion=false
     else
         keepMajorMillVersion=true
     fi
-    if [ -f ".mill-version" ] ; then
-        rm -rf "${XDG_CACHE_HOME:-$HOME/.cache}"/p10k-${(%):-%n}/millversion/latest_mill_version
-        latest_mill_version=$(curl -sL https://repo1.maven.org/maven2/com/lihaoyi/mill-scalalib_2.13/maven-metadata.xml | grep "<version>" |grep -v "\-M" |tail -1 |sed -e 's/<[^>]*>//g' |tr -d " ")
+    if [ -f ".mill-version" ]; then
+        rm -rf "${XDG_CACHE_HOME:-$HOME/.cache}"/p10k-${USER}/millversion/latest_mill_version
+        latest_mill_version=$(curl -sL https://repo1.maven.org/maven2/com/lihaoyi/mill-scalalib_2.13/maven-metadata.xml | grep "<version>" | grep -v "\-M" | tail -1 | sed -e 's/<[^>]*>//g' | tr -d " ")
         echo "Latest mill version is $latest_mill_version..."
         if [ "$keepMajorMillVersion" = true ]; then
             latest_mill_version=$(echo "$latest_mill_version" | cut -d- -f1)
@@ -93,11 +91,11 @@ millupd() {
         millver=$(cat .mill-version || echo 'bug')
         if [[ -n "$latest_mill_version" && "$millver" != "$latest_mill_version" ]]; then
             echo "Version differs, currently in $millver... updating .mill-version to $latest_mill_version."
-            echo "$latest_mill_version" > .mill-version
+            echo "$latest_mill_version" >.mill-version
         else
             echo "Mill is already up-to-date."
         fi
     else
-      return
+        return
     fi
 }
